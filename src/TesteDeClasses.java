b28ad0d11b430b98c704106a9462f7f56a63eff6
Rogério <rogerio.ferreira@manyminds.com.br>
2021-07-24 15:28:31 -0300

    Primeiro commit com a classe de atributos e configuracoes

--- src/TesteDeClasses.java
+++ src/TesteDeClasses.java
@@ -2,6 +2,7 @@ public class TesteDeClasses {
 
     public static void main(String[] args) {
         Atributos at1 = new Atributos();
+
         at1.setNome("Força");
         at1.setValorDoAtributo(-1);
         System.out.println("O valor do de "+at1.getNome()+" o valor do atributo é "+at1.getValorDoAtributo()+" modificador é :"+at1.getModificador());
